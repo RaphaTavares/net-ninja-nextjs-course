14afec161ca38132e4c267bdf6800e79bb41336d
Raphael <raphael_tavares2000@hotmail.com>
2024-11-03 12:58:09 -0300

    Lesson 11 - add create ticket form

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from "./CreateForm"
+
+export default function CreateTickets() {
+  return (
+    <main>
+        <h2 className="text-primary text-center">Add a new Ticket</h2>
+        <CreateForm />
+    </main>
+)
+}
