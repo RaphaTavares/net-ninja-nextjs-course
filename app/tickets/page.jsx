23fca30c5922944d857448489778b38b69ceba48
Raphael <raphael_tavares2000@hotmail.com>
2024-11-04 18:49:55 -0300

    post course: add button to create ticket page

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,21 +1,37 @@
 import { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "../loading";
+import Link from "next/link"
 
 export default function Tickets() {
-    return (
-      <main>
-        <nav>
-          <div>
-            <h2>Tickets</h2>
-            <p><small>Currently open tickets.</small></p>
-          </div>
-        </nav>
 
-        <Suspense fallback={<Loading />}>
-          <TicketList />
-        </Suspense>
-      </main>
-    )
-  }
-  
+  return (
+    <main>
+      <nav style={{ position: "relative" }}>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Currently open tickets.</small></p>
+        </div>
+        <Link href={`/tickets/create`}>
+        <button
+        className="btn-primary"
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            padding: "8px 12px",
+            fontSize: "16px",
+            cursor: "pointer"
+          }}
+        >
+          New Ticket
+        </button>
+        </Link>
+      </nav>
+
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
+    </main>
+  );
+}
